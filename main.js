cb8afea4493780ba157601a820a82d5c4a2d7100
Matteo <trombinimatteo@gmail.com>
2021-12-19 18:50:37 +0000

    Updated: Planet colors

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-// var ballRadius = 10;
-// var orbitHeight = 100;
 var dtheta = 0.01;
 x = canvas.width / 2;
 y = canvas.height / 2;
-// var x_orbit = (orbitHeight * Math.cos(theta)) + x;
-// var y_orbit = (orbitHeight * Math.sin(theta)) + y;
-// var dtheta = -0.01;
-// var ballColor = 'red';
 
 class Planet {
     constructor(orbitHeight, theta, dtheta, radius, color) {
@@ -27,21 +21,21 @@ class Planet {
     }
 }
 
-var earth = new Planet(100, 0, -0.001, 10, 'blue');
-var mars = new Planet(250, 0, -0.0015, 7, 'red');
-var jupiter = new Planet(380, 0, -0.0002, 18, 'orange')
+var earth = new Planet(100, 0, -0.001, 10, '#3260a8');
+var mars = new Planet(250, 0, -0.0015, 7, '#a84e32');
+var jupiter = new Planet(380, 0, -0.0002, 18, '#c48114')
 
 // console.log(earth.theta);
 function drawSun() {
     ctx.beginPath();
     ctx.arc(x, y, 45, 0, Math.PI * 2);
-    ctx.fillStyle = 'yellow';
+    ctx.fillStyle = '#d3d921';
     ctx.fill();
     ctx.closePath();
 }
 
 
-function drawBall(x1, y1, r1, color1) {
+function drawPlanet(x1, y1, r1, color1) {
     ctx.beginPath();
     ctx.arc(x1, y1, r1, 0, Math.PI * 2);
     ctx.fillStyle = color1;
@@ -55,9 +49,9 @@ function draw() {
     earth.theta += earth.dtheta;
     mars.theta += mars.dtheta;
     jupiter.theta += jupiter.dtheta;
-    drawBall(earth.calcPos()[0], earth.calcPos()[1], earth.radius, earth.color);
-    drawBall(mars.calcPos()[0], mars.calcPos()[1], mars.radius, mars.color);
-    drawBall(jupiter.calcPos()[0], jupiter.calcPos()[1], jupiter.radius, jupiter.color);
+    drawPlanet(earth.calcPos()[0], earth.calcPos()[1], earth.radius, earth.color);
+    drawPlanet(mars.calcPos()[0], mars.calcPos()[1], mars.radius, mars.color);
+    drawPlanet(jupiter.calcPos()[0], jupiter.calcPos()[1], jupiter.radius, jupiter.color);
 }
 
 setInterval(draw, 10);
