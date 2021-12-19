7a734307284d5707cc382c8078d73239ab10dbe8
Matteo <trombinimatteo@gmail.com>
2021-12-19 17:25:37 +0000

    Added: Classes

--- main.js
+++ main.js
@@ -1,31 +1,49 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-var dx = 2;
-var dy = -2;
+// var ballRadius = 10;
+// var orbitHeight = 100;
+var dtheta = 0.01;
+x = canvas.width / 2;
+y = canvas.height / 2;
+// var x_orbit = (orbitHeight * Math.cos(theta)) + x;
+// var y_orbit = (orbitHeight * Math.sin(theta)) + y;
+// var dtheta = -0.01;
+// var ballColor = 'red';
+
+class Planet {
+    constructor(orbitHeight, theta, radius, color) {
+        this.orbitHeight = orbitHeight;
+        this.theta = theta;
+        this.dtheta = dtheta;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    calcPos() {
+        const coords = [0, 0];
+        coords[0] = this.orbitHeight * Math.cos(this.theta) + x;
+        coords[1] = this.orbitHeight * Math.sin(this.theta) + y;
+        return coords;
+    }
+}
+
+var earth = new Planet(100, 0, 10, 'blue');
+
+console.log(earth.theta);
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(earth.calcPos()[0], earth.calcPos()[0], earth.radius, 0, Math.PI * 2);
+    ctx.fillStyle = earth.color;
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    earth.theta += dtheta;
+    console.log(earth.theta);
     drawBall();
-    x += dx;
-    y += dy;
-    if (x + dx > canvas.width || x + dx < 0) {
-        dx = -dx;
-    }
-
-    if (y + dy > canvas.height || y + dy < 0) {
-        dy = -dy;
-    }
-
 }
 
 setInterval(draw, 10);
